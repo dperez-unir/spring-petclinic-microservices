46ca67a2b7941054074b48d4fed7b5f22c1b1c10
David Pérez <dperez@loycus.com>
2024-09-15 18:07:17 +0200

    Se añaden variables de entorno en el build de jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,17 @@
 pipeline {
     agent any
     stages {
+        stage('Check Environment Variables') {
+            steps {
+                script {
+                    // Imprime todas las variables de entorno
+                    sh 'env'
+
+                    // O si prefieres filtrar específicamente por Docker
+                    // sh 'env | grep DOCKER'
+                }
+            }
+        }
         stage('Build') {
             steps {
                 sh 'mvn clean install -PbuildDocker'
