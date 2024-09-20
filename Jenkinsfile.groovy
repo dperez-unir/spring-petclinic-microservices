9e6c6158325ada62431722f8391550e11649a3a0
David Pérez Rodríguez <dperez@zertiban.com>
2024-09-20 18:26:31 +0200

    Se cambia color tema

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,20 +1,29 @@
 pipeline {
     agent any
-    stages {
-        stage('Check Environment Variables') {
+        stage('Update Maven Versions') {
             steps {
                 script {
-                    // Imprime todas las variables de entorno
-                    sh 'env'
+                    // Usa el plugin de Maven Versions para cambiar la versión de todos los poms
+                    sh 'mvn versions:set -DnewVersion=`mvn help:evaluate -Dexpression=project.version | grep -v "\\[INFO\\]" | awk -F \'.\' \'{print $1"."$2"."$3+1}\'`'
 
-                    // O si prefieres filtrar específicamente por Docker
-                    // sh 'env | grep DOCKER'
+                    // Commit los cambios con un mensaje de commit adecuado
+                    sh 'git commit -am "Subida de versión"'
                 }
             }
         }
+
         stage('Build') {
             steps {
                 sh 'mvn clean install -PbuildDocker'
+            }
+        }
+
+        stage('Push Changes') {
+            steps {
+                script {
+                    // Empuja los cambios al repositorio
+                    sh 'git push origin HEAD'
+                }
             }
         }
     }
