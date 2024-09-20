15243e179fc78e727562043960f4a5374d32af8e
David Pérez Rodríguez <dperez@zertiban.com>
2024-09-20 19:04:32 +0200

    Cambio de color

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,16 +5,14 @@ pipeline {
         stage('Update Maven Versions') {
             steps {
                 script {
-                    // Usa el plugin de Maven Versions para cambiar la versión de todos los poms
                     sh '''
+                        git config --global user.email "you@example.com"
+                        git config --global user.name "Tu Nombre"
                         currentVersion=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout) && \
                         newVersion=$(echo $currentVersion | awk -F "." \'{print $1"."$2"."$3+1}\') && \
                         find . -name "pom.xml" -exec sed -i "s/<version>$currentVersion<\\/version>/<version>$newVersion<\\/version>/" {} +
+                        git commit -am "Subida de versión"
                     '''
-
-
-                    // Commit los cambios con un mensaje de commit adecuado
-                    sh 'git commit -am "Subida de versión"'
                 }
             }
         }
