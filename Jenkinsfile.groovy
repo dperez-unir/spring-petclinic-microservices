d0d3a7420e0cb6ed0ed6cbf24761242d50ab314c
David Pérez Rodríguez <dperez@zertiban.com>
2024-09-29 13:06:40 +0200

    TFM-Presentación: Se filtra último commit para evitar caer en bucle

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,32 @@
 pipeline {
     agent any
     stages {
+        stage('Check Last Commit Message') {
+            steps {
+                script {
+                    def lastCommitMessage = ""
+                    def changeLogSets = currentBuild.changeSets
 
+                    // Recorremos los cambios pero solo tomamos el primer commit (el más reciente)
+                    for (int i = 0; i < changeLogSets.size(); i++) {
+                        def entries = changeLogSets[i].items
+                        if (entries.length > 0) {
+                            // Recuperamos solo el mensaje del primer commit más reciente
+                            lastCommitMessage = entries[0].msg
+                            break
+                        }
+                    }
+
+                    echo "Último mensaje de commit: ${lastCommitMessage}"
+
+                    // Verificamos si contiene la frase que deseas ignorar
+                    if (lastCommitMessage.contains("subida de version Jenkins")) {
+                        currentBuild.result = 'ABORTED'
+                        error("Build aborted due to Jenkins auto-commit.")
+                    }
+                }
+            }
+        }
         stage('Update Maven Versions') {
             steps {
                 script {
@@ -29,7 +54,7 @@ pipeline {
                     // Empuja los cambios al repositorio
                     withCredentials([usernamePassword(credentialsId: '1d65426d-86d5-4860-af62-6afbcbb50317', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
                         sh 'git add .'
-                        sh 'git commit -am "subida de version"'
+                        sh 'git commit -am "subida de version Jenkins"'
                         sh 'git config user.email "tu-email@ejemplo.com"'
                         sh 'git config user.name "Tu Nombre"'
                         sh 'git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/dperez-unir/spring-petclinic-microservices.git HEAD:main'
